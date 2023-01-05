9d71cf8966ed0697b92a6f175514ad5ab156f9d9
Marcelearn <jaworskimarcel@gmail.com>
2023-01-05 14:07:01 +0100

    queries.sql

---
+++ queries_db.sql
@@ -0,0 +1,13 @@
+SELECT * FROM account;
+/*
+SELECT * FROM acc_transaction;
+SELECT * FROM branch;
+SELECT * FROM business;
+SELECT * FROM customer;
+SELECT * FROM individual;
+SELECT * FROM product;
+*/
+
+SELECT txn_id, amount, account_id FROM acc_transaction;
+
+
