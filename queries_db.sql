7108542f8128006d8b2b1ce323c8b0c8fe5d5764
Marcelearn <jaworskimarcel@gmail.com>
2023-01-05 14:41:36 +0100

    EXERCISES

--- queries_db.sql
+++ queries_db.sql
@@ -1,4 +1,5 @@
-SELECT * FROM account;
+SELECT * 
+FROM account;
 /*
 SELECT * FROM acc_transaction;
 SELECT * FROM branch;
@@ -7,7 +8,44 @@ SELECT * FROM customer;
 SELECT * FROM individual;
 SELECT * FROM product;
 */
-
-SELECT txn_id, amount, account_id FROM acc_transaction;
+/* 4 */
+SELECT txn_id, amount, account_id 
+FROM acc_transaction;
+/* 5 */
+SELECT address, city 
+FROM branch
+LIMIT 3;
+/* 6 */
+SELECT name, incorp_date
+FROM business
+ORDER BY incorp_date ASC;
+/* 7 */
+SELECT fed_id, cust_id
+FROM customer
+WHERE state LIKE "nh";
+/* 8 */
+SELECT account_id, last_activity_date
+FROM account
+WHERE pending_balance<1000;
+/* 9 */
+SELECT account_id, last_activity_date
+FROM account
+WHERE pending_balance BETWEEN 1000 AND 3000;
+/* 10 */
+SELECT first_name, last_name
+FROM employee
+WHERE title="manager";
+/* 11 */
+SELECT product_type_cd
+FROM product;
+/* 12 */
+SELECT AVG(avail_balance)
+FROM account;
+/* 13 */
+SELECT MAX(avail_balance)
+FROM account;
+/* 14 */
+SELECT MAX(avail_balance)
+FROM account;
 
 
